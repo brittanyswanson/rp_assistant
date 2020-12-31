b1c93ae6a08f38d5a42f15ea4b6dc18ecf954887
Brittany Swanson <brittanyjswanson@gmail.com>
2020-12-31 10:00:06 -0600

    Updated logging & added faceclaim

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ import mysql.connector
 import logging
 
 # Logging Setup
-logging.basicConfig(filename="bot.log", 
-                    format='%(asctime)s %(message)s', 
-                    filemode='w') 
+logging.basicConfig(filename=time.strftime('bot-%Y-%m-%d.log'),
+                    level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    filemode='a')
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
 
 # Connecting using Discord Token
@@ -22,18 +22,21 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 # Database Work
+
+
 def connect_to_DB():
+    # Get credentials from credentials file
     config = configparser.ConfigParser()
     config.read('env.ini')
 
     if 'mysql' in config:
         creds = config['mysql']
         mydb = mysql.connector.connect(
-            host = creds['host'],
-            database = creds['db'],
-            user = creds['user'],
-            passwd = creds['passwd'],
-            auth_plugin = "mysql_native_password")
+            host=creds['host'],
+            database=creds['db'],
+            user=creds['user'],
+            passwd=creds['passwd'],
+            auth_plugin="mysql_native_password")
 
         try:
             if (mydb):
@@ -44,13 +47,14 @@ def connect_to_DB():
             if status == "connection successful":
                 logger.debug("Connected to DB")
                 return mydb
-        
+
         except Exception as e:
             logger.error("Fucked up trying to connect. " + str(e))
             status = "Fucked up trying to connect.  Error:  %s" % str(e)
 
     else:
         logger.error("Failure reading mysql credentials.")
+
 
 def query_db_no_param(query):
     mydb = connect_to_DB()
@@ -64,7 +68,7 @@ def query_db_no_param(query):
             mydb.ping(True)
             cursor = mydb.cursor()
             cursor.execute(query)
-            
+
             for row in cursor:
                 for field in row:
                     row_list.append(field)
@@ -76,13 +80,14 @@ def query_db_no_param(query):
     finally:
         cursor.close()
         mydb.close()
-        
+
         if len(row_list) > 0:
             return row_list
         else:
             row_list = ['empty']
-            logger.debug("No results from query: " + str(query))
+            logger.info("No results from query: " + str(query))
             return row_list
+
 
 def query_db(query):
     mydb = connect_to_DB()
@@ -97,7 +102,7 @@ def query_db(query):
             cursor = mydb.cursor()
             # Changed to put * before query in order to unpack.  Was getting a system error.
             cursor.execute(*query)
-            
+
             for row in cursor:
                 for field in row:
                     row_list.append(field)
@@ -109,19 +114,19 @@ def query_db(query):
     finally:
         cursor.close()
         mydb.close()
-        
+
         if len(row_list) > 0:
             return row_list
         else:
             row_list = ['empty']
-            logger.debug("No results from query: " + str(query))
+            logger.info("No results from query: " + str(query))
             return row_list
 
-        # TODO: Is this still necessary
+        # TODO: Is this still necessary?
         if row is not None:
-            logger.debug("*****This is still being used, clearly! Line 120**********")
+            logger.debug(
+                "*****This is still being used, clearly! Line 120**********")
             return row
-
 
 
 @bot.event
@@ -129,11 +134,10 @@ async def on_ready():
     logger.info(f'{bot.user.name} has connected to Discord!')
 
 
-
 # -----------------
 # Help
 # -----------------
-@bot.command(name='helpme', help = 'Input: None     Output: help')
+@bot.command(name='helpme', help='Input: None     Output: help')
 async def show_help(ctx):
     logger.info('!helpme was called.')
     await ctx.send('Always begin a command with an exclamanation(!).\nList of commands available:\n\n**!species** - Returns a list of all available species. \n**!char_list vampire** - Lists active characters of a species (must input a species name)\n**!chars "mysteryuser"** - Lists active characters for a player (use player\'s name in double quotes if multiple words)\n**!app "marty mcfly"** - Shows the application url (and other goodies) for a character (use character\'s name in quotes)\n\nFor more information, visit www.rpconsole.com/bot.html')
@@ -141,6 +145,8 @@ async def show_help(ctx):
 # -----------------
 # Roll Dice
 # -----------------
+
+
 @bot.command(name='roll_dice', help='Call it with the number of dice and number of sides separated by spaces')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     logger.info('!roll_dice was called.  Who even knew about this?')
@@ -154,30 +160,37 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
 # -----------------
 # !app "<name>"
 # -----------------
-@bot.command(name='app', help = 'Input: full character name    Output: character bio url')
-async def get_character_url(ctx, char_name:str):
+@bot.command(name='app', help='Input: full character name    Output: character bio url')
+async def get_character_url(ctx, char_name: str):
 
     logger.info('!app was called with parameter: ' + char_name)
 
     proper_character_name = char_name.lower()
-    char_query = """SELECT name, species, player_name, url FROM characters WHERE name = %(name)s""", {'name': proper_character_name}
+    char_query = """SELECT name, species, player_name, url FROM characters WHERE name = %(name)s""", {
+        'name': proper_character_name}
     logger.debug(char_query)
 
     try:
         data = query_db(char_query)
         await ctx.send("character name: " + data[0] + "\n" + "species: " + data[1] + "\n" + "player: " + data[2] + "\n" + "application: " + data[3])
     except:
-        logger.error("An error happened in get_character_url using: " + char_name)
+        logger.error(
+            "An error happened in get_character_url using: " + char_name)
 
 # -----------------
 # !char_list <species>
 # -----------------
-@bot.command(name='char_list', help = 'Input: species name    Output: list of characters in species')
-async def get_characters_in_species(ctx, species:str):
+
+
+@bot.command(name='char_list', help='Input: species name    Output: list of characters in species')
+async def get_characters_in_species(ctx, species: str):
     logger.info('!char_list was called with parameter: ' + species)
 
+    # TO-DO: Check to see if someone made this plural
+    # Only allowable species are: aether, dryad, fae, harpy, human, hydra, kemuri, merfolk, nuk, shifter, sphinx, uraei, vampire, werewolf, whitestag, witch
     proper_species_name = species.lower()
-    char_query = """SELECT name FROM characters WHERE species = %(species)s AND active = 'Y' ORDER BY name ASC""", {'species': proper_species_name}
+    char_query = """SELECT name FROM characters WHERE species = %(species)s AND active = 'Y' ORDER BY name ASC""", {
+        'species': proper_species_name}
 
     data_list = query_db(char_query)
 
@@ -186,13 +199,19 @@ async def get_characters_in_species(ctx, species:str):
 # -----------------
 # !chars <player>
 # -----------------
-@bot.command(name='chars', help = 'Input: player name    Output: list of characters by player')
-async def get_characters_from_player(ctx, player:str):
-    logger.info('!char_list was called with parameter: ' + player)
+
+
+@bot.command(name='chars', help='Input: player name    Output: list of characters by player')
+async def get_characters_from_player(ctx, player: str):
+    logger.info('!chars was called with parameter: ' + player)
     proper_player_name = player.lower()
-    char_query = """SELECT name FROM characters WHERE player_name = %(player)s AND active = 'Y' ORDER BY name ASC""", {'player': proper_player_name}
+    char_query = """SELECT name FROM characters WHERE player_name = %(player)s AND active = 'Y' ORDER BY name ASC""", {
+        'player': proper_player_name}
 
     data_list = query_db(char_query)
+
+    # Check if data_list is empty
+    # If empty, print out a message about no player with that name.
 
     await ctx.send("-------------------\n" + "**" + player + "'s characters**\n" + "-------------------\n" + "\n".join(map(str, data_list)))
 
@@ -200,7 +219,7 @@ async def get_characters_from_player(ctx, player:str):
 # -----------------
 # Get species
 # -----------------
-@bot.command(name='species', help = 'Input: None     Output: list of species')
+@bot.command(name='species', help='Input: None     Output: list of species')
 async def get_species(ctx):
     logger.info('!species was called.')
     char_query = """SELECT distinct species FROM characters"""
@@ -209,5 +228,27 @@ async def get_species(ctx):
     data_list = query_db_no_param(char_query)
 
     await ctx.send('**Available Species**\n' + '\n'.join(map(str, data_list)))
+
+bot.run(TOKEN)
+
+# -----------------
+# Check if faceclaim is available
+# -----------------
+
+
+@bot.command(name='faceclaim', help='Input: None     Output: Says whether name is in use')
+async def get_faceclaim(ctx, faceclaim):
+    logger.info('!faceclaim was called.')
+    fc_query = """SELECT count(name) FROM characters WHERE faceclaim = %(faceclaim)s AND active = 'Y'""", {
+        'faceclaim': faceclaim}
+
+    fc_records = query_db(fc_query)
+
+    if fc_records > 0:
+        fc_status = "taken"
+    else:
+        fc_status = "available"
+
+    await ctx.send('** ' + fc_status + ' **\n')
 
 bot.run(TOKEN)

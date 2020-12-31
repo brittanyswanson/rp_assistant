9f15eabfea47b95dd565fa51819451424850bf2a
Brittany Swanson <brittanyjswanson@gmail.com>
2020-12-31 10:51:01 -0600

    Logging and formatting and error messages

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import configparser
 from discord.ext import commands
 import mysql.connector
 import logging
+import time
 
 # Logging Setup
 logging.basicConfig(filename=time.strftime('bot-%Y-%m-%d.log'),
@@ -22,8 +23,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 # Database Work
-
-
 def connect_to_DB():
     # Get credentials from credentials file
     config = configparser.ConfigParser()
@@ -118,7 +117,6 @@ def query_db(query):
         if len(row_list) > 0:
             return row_list
         else:
-            row_list = ['empty']
             logger.info("No results from query: " + str(query))
             return row_list
 
@@ -128,7 +126,11 @@ def query_db(query):
                 "*****This is still being used, clearly! Line 120**********")
             return row
 
-
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
+#                                Bot Code Starts Here
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
 @bot.event
 async def on_ready():
     logger.info(f'{bot.user.name} has connected to Discord!')
@@ -145,8 +147,6 @@ async def show_help(ctx):
 # -----------------
 # Roll Dice
 # -----------------
-
-
 @bot.command(name='roll_dice', help='Call it with the number of dice and number of sides separated by spaces')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     logger.info('!roll_dice was called.  Who even knew about this?')
@@ -166,22 +166,22 @@ async def get_character_url(ctx, char_name: str):
     logger.info('!app was called with parameter: ' + char_name)
 
     proper_character_name = char_name.lower()
-    char_query = """SELECT name, species, player_name, url FROM characters WHERE name = %(name)s""", {
-        'name': proper_character_name}
+    char_query = """SELECT name, species, player_name, url FROM characters WHERE name = %s""", (proper_character_name,)
     logger.debug(char_query)
 
     try:
         data = query_db(char_query)
-        await ctx.send("character name: " + data[0] + "\n" + "species: " + data[1] + "\n" + "player: " + data[2] + "\n" + "application: " + data[3])
+
+        if len(data) < 1:
+            await ctx.send("Hmmm.  I can't find a character with that name.  Maybe try !char_list to see what characters are available by species.")
+        else:
+            await ctx.send("character name: " + data[0] + "\n" + "species: " + data[1] + "\n" + "player: " + data[2] + "\n" + "application: " + data[3])
     except:
-        logger.error(
-            "An error happened in get_character_url using: " + char_name)
+        logger.error("An error happened in get_character_url using: " + char_name)
 
 # -----------------
 # !char_list <species>
 # -----------------
-
-
 @bot.command(name='char_list', help='Input: species name    Output: list of characters in species')
 async def get_characters_in_species(ctx, species: str):
     logger.info('!char_list was called with parameter: ' + species)
@@ -194,26 +194,27 @@ async def get_characters_in_species(ctx, species: str):
 
     data_list = query_db(char_query)
 
-    await ctx.send("-------------------\n" + "**" + species + " characters**\n" + "-------------------\n" + "\n".join(map(str, data_list)))
+    if len(data_list) < 1:
+        await ctx.send("We all make mistakes.\nNo results found.  Check for mispellings or try the command !species to see availble options.")
+    else:
+        await ctx.send("-------------------\n" + "**" + species + " characters**\n" + "-------------------\n" + "\n".join(map(str, data_list)))
+    
 
 # -----------------
 # !chars <player>
 # -----------------
-
-
 @bot.command(name='chars', help='Input: player name    Output: list of characters by player')
 async def get_characters_from_player(ctx, player: str):
     logger.info('!chars was called with parameter: ' + player)
     proper_player_name = player.lower()
-    char_query = """SELECT name FROM characters WHERE player_name = %(player)s AND active = 'Y' ORDER BY name ASC""", {
-        'player': proper_player_name}
+    char_query = """SELECT name FROM characters WHERE player_name = %s AND active = 'Y' ORDER BY name ASC""", (proper_player_name,)
 
     data_list = query_db(char_query)
 
-    # Check if data_list is empty
-    # If empty, print out a message about no player with that name.
-
-    await ctx.send("-------------------\n" + "**" + player + "'s characters**\n" + "-------------------\n" + "\n".join(map(str, data_list)))
+    if len(data_list) < 1:
+        await ctx.send("This is awkward as hell...I can't find this player.  Are you sure that's there player name?")
+    else:
+        await ctx.send("-------------------\n" + "**" + player + "'s characters**\n" + "-------------------\n" + "\n".join(map(str, data_list)))
 
 
 # -----------------
@@ -229,26 +230,24 @@ async def get_species(ctx):
 
     await ctx.send('**Available Species**\n' + '\n'.join(map(str, data_list)))
 
-bot.run(TOKEN)
+
 
 # -----------------
-# Check if faceclaim is available
+# !faceclaim "fc name"
 # -----------------
-
-
-@bot.command(name='faceclaim', help='Input: None     Output: Says whether name is in use')
+@bot.command(name='faceclaim', help='Input: Face Claim in quotes    Output: Says whether name is in use')
 async def get_faceclaim(ctx, faceclaim):
-    logger.info('!faceclaim was called.')
-    fc_query = """SELECT count(name) FROM characters WHERE faceclaim = %(faceclaim)s AND active = 'Y'""", {
-        'faceclaim': faceclaim}
+    logger.info('!faceclaim was called for ' + faceclaim)
+
+    fc_query = """SELECT name FROM characters WHERE faceclaim = %s AND active = 'Y'""", (faceclaim,)
 
     fc_records = query_db(fc_query)
 
-    if fc_records > 0:
+    if len(fc_records) > 0:
         fc_status = "taken"
     else:
         fc_status = "available"
 
-    await ctx.send('** ' + fc_status + ' **\n')
+    await ctx.send('** ' + faceclaim + ' is ' + fc_status + ' **\n')
 
 bot.run(TOKEN)
